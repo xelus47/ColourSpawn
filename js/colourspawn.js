7fc51b4c162fec1991ddef6afe9b07b2dd38b571
xelus47 <aleksei0kozlov@gmail.com>
2017-03-08 23:12:54 +0100

    added hex

--- js/colourspawn.js
+++ js/colourspawn.js
@@ -1,3 +1,16 @@
+// courtesy of http://snipplr.com/view/37973/
+
+function rgb2hex(r,g,b) {
+  var rgb = [r.toString(16),g.toString(16),b.toString(16)]
+  for (var i=0;i<3;i++) {
+    if (rgb[i].length==1) rgb[i]=rgb[i]+rgb[i];
+  }
+  if(rgb[0][0]==rgb[0][1] && rgb[1][0]==rgb[1][1] && rgb[2][0]==rgb[2][1])
+    return '#'+rgb[0][0]+rgb[1][0]+rgb[2][0];
+  return '#'+rgb[0]+rgb[1]+rgb[2];
+}
+
+
 function lisa(event){
 	//console.log(event);
 	if (event.keyCode==13){
@@ -5,8 +18,9 @@ function lisa(event){
 		g=parseInt(Math.random()*256);
 		b=parseInt(Math.random()*256);
 		code = "rgb("+r+","+g+","+b+")"
+		hex = rgb2hex(r,g,b)
 		document.body.parentNode.style.backgroundColor = code;
-		document.getElementById('colour_name').innerHTML=code + " ";
+		document.getElementById('colour_name').innerHTML=code + "<br />" + hex + " ";
 	} else if (49<=event.charCode<=122) {
 		chara = String.fromCharCode(event.charCode);
 		document.getElementById('colour_name').innerHTML+=chara;
